cb8843eeda663b2633b15b977bf8f952067392ba
Connor Adams <connorads@users.noreply.github.com>
2021-07-04 23:03:41 +0100

    Rename environment variable to Haction

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import * as env from 'env-var';
 import {generateFauxRepoActivity} from './lib';
 
 (async () => {
-  if (env.get('GITHUB_HACTION_ENABLED').asBool()) {
+  if (env.get('HACTION_ENABLED').asBool()) {
     await generateFauxRepoActivity();
   } else {
     console.log("They'll do nuffink");
